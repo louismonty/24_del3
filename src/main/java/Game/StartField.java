eaf2b1cbb1f4d28db4621097e364928b871d74ff
Isak_s205452 <s205452@student.dtu.dk>
2020-11-08 14:25:29 +0100

    Created StartField class & added constructor

---
+++ src/main/java/Game/StartField.java
@@ -0,0 +1,11 @@
+package Game;
+
+public class StartField extends Field
+{
+    private int startBonus;
+    public StartField(int positionID, String colorID, String name)
+    {
+        super(positionID, colorID, name);
+        this.startBonus = 2;
+    }
+}
