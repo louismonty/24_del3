5f7a00335e452b6ec73a0a9a60c4d02fcbfdb3ed
Daniel <danieltsutton@outlook.com>
2020-11-09 12:31:19 +0100

    PlayerController moved to controller folder

--- src/main/java/Controller/PlayerController.java
+++ src/main/java/Controller/PlayerController.java
@@ -1,4 +1,23 @@
 package Controller;
 
+import Game.Account;
+import Game.Player;
+
 public class PlayerController {
+
+    public Player getPlayer()
+    {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    private Player player;
+
+    public void CreatePlayer()
+    {
+        player = new Player("UNKNOWN", 0, new Account(1));
+    }
 }
