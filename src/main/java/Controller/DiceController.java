b7b25af19a6b133bb3260cbdcdae8f8c4d105f39
louismonty <louis.mail.dk@gmail.com>
2020-11-13 10:38:05 +0100

    fixed to only have one dice

--- src/main/java/Controller/DiceController.java
+++ src/main/java/Controller/DiceController.java
@@ -4,17 +4,15 @@ import Game.Dice;
 
 public class DiceController {
     private Dice dice1 = new Dice();
-    private Dice dice2 = new Dice();
 
     public void DiceRoll(){
         dice1.roll();
-        dice2.roll();
     }
     public int DiceValue(){
-        return dice1.getFaceValue()+dice2.getFaceValue();
+        return dice1.getFaceValue();
     }
     public String toString(){
-        return "Du slog"+dice1.getFaceValue()+" "+dice2.getFaceValue();
+        return "Du slog"+dice1.getFaceValue();
 
     }
 
