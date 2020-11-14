8d4a2230acf56aa72c4da21fecd4710b424fdc6c
Isak_s205452 <s205452@student.dtu.dk>
2020-11-14 18:17:50 +0100

    Filled startCard and fiveFieldsForwardCard method in StartField class

--- src/main/java/Game/ChanceCard.java
+++ src/main/java/Game/ChanceCard.java
@@ -1,4 +1,5 @@
 package Game;
+import java.util.Scanner;
 
 public class ChanceCard
 {
@@ -10,20 +11,32 @@ public class ChanceCard
         chanceCardID = id;
         chanceCardName = name;
     }
-
+/* Needs player color and next turn option
     public void carCard()
     {
+        if(Player.color == "Green")
 
     }
 
+ */
+
     public void startCard()
     {
-
+        StartField.addStartBonus();
     }
 
     public void fiveFieldsForwardCard()
     {
-
+        int fieldsToMove;
+        Scanner chanceScan = new Scanner(System.in);
+        System.out.println("Du kan rykke op til 5 felter frem. Indtast hvor mange felter du ønsker at rykke:");
+        fieldsToMove = chanceScan.nextInt();
+        while(fieldsToMove<1 || fieldsToMove>5)
+        {
+            System.out.println("Indtast et tal mellem 1-5");
+            fieldsToMove = chanceScan.nextInt();
+        }
+        newPlayerPosition(fieldsToMove);
     }
 
 }
