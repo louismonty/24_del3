e25ebd542437b4e6660213e2bc6aff566fbb2eff
louismonty <louis.mail.dk@gmail.com>
2020-11-15 10:01:40 +0100

    change dice to only one dice

--- src/main/java/Controller/DiceController.java
+++ src/main/java/Controller/DiceController.java
@@ -3,23 +3,22 @@ package Controller;
 import Game.Dice;
 
 public class DiceController {
-    private Dice dice1 = new Dice();
-    private Dice dice2 = new Dice();
+    private static Dice dice1 = new Dice();
+
 
 
     /**
      * rolls the 2 dice
      * to get value use DiceValue()
      */
-    public void DiceRoll(){
+    public static void diceRoll(){
         dice1.roll();
-        dice2.roll();
     }
-    public int DiceValue(){
-        return dice1.getFaceValue()+dice2.getFaceValue();
+    public int diceValue(){
+        return dice1.getFaceValue();
     }
     public String toString(){
-        return "Du slog"+dice1.getFaceValue()+" "+dice2.getFaceValue();
+        return "Du slog"+" "+dice1.getFaceValue();
 
     }
 
