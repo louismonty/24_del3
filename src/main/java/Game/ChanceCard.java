61db6c70e540714d9e9984cd547f8481f70c1352
Isak_s205452 <s205452@student.dtu.dk>
2020-11-17 23:08:47 +0100

    added freeOrangeOrGreenCard and freePinkOrBlueCard methods and renamed free"color"Fields methods to free"color"Card in ChanceCard class

--- src/main/java/Game/ChanceCard.java
+++ src/main/java/Game/ChanceCard.java
@@ -55,27 +55,81 @@ public class ChanceCard
         }
     }
 
-    public void freeOrangeField(Player currentPlayer, PropertyField currentField, GUIController GUIController)
+    public void freeOrangeCard(Player currentPlayer, PropertyField currentField, GUIController GUIController)
     {
-        if(GUIController.twoButtons("Gratis felt! Ryk frem til et orange felt. Hvis det er ledigt, får du det gratis! Ellers skal du betale leje til ejeren", "Burgerbaren", "Pizzariaet").equals("Burgerbaren") ==true)
-            currentPlayer.setPlayerPosition(1);
-        else currentPlayer.setPlayerPosition(2);
+        if(GUIController.twoButtons("Gratis felt! Ryk frem til et orange felt. " +
+                "\nHvis det er ledigt, får du det gratis! " +
+                "\nEllers skal du betale leje til ejeren", "Skaterparken", "Swimmingpoolen").equals("Skaterparken") ==true)
+            currentPlayer.setPlayerPosition(10);
+        else currentPlayer.setPlayerPosition(11);
         checkIfPropertyIsBought(currentPlayer, currentField);
     }
 
-    public void freeLightBlueField(Player currentPlayer, PropertyField currentField, GUIController GUIController)
+    public void freeLightBlueCard(Player currentPlayer, PropertyField currentField, GUIController GUIController)
     {
-        if(GUIController.twoButtons("Gratis felt! Ryk frem til et lyseblåt felt. Hvis det er ledigt, får du det gratis! Ellers skal du betale leje til ejeren", "Slikbutikken", "Iskiosken").equals("Burgerbaren") ==true)
+        if(GUIController.twoButtons("Gratis felt! Ryk frem til et lyseblåt felt. " +
+                "\nHvis det er ledigt, får du det gratis! " +
+                "\nEllers skal du betale leje til ejeren", "Slikbutikken", "Iskiosken").equals("Slikbutikken") ==true)
             currentPlayer.setPlayerPosition(4);
         else currentPlayer.setPlayerPosition(5);
         checkIfPropertyIsBought(currentPlayer, currentField);
     }
 
-    public void freeRedField(Player currentPlayer, PropertyField currentField, GUIController GUIController)
+    public void freeRedCard(Player currentPlayer, PropertyField currentField, GUIController GUIController)
     {
-        if(GUIController.twoButtons("Gratis felt! Ryk frem til et orange felt. Hvis det er ledigt, får du det gratis! Ellers skal du betale leje til ejeren", "Spillehallen", "Biografen").equals("Burgerbaren") ==true)
+        if(GUIController.twoButtons("Gratis felt! Ryk frem til et rødt felt. " +
+                "\nHvis det er ledigt, får du det gratis! " +
+                "\nEllers skal du betale leje til ejeren", "Spillehallen", "Biografen").equals("Spillehallen") ==true)
             currentPlayer.setPlayerPosition(13);
         else currentPlayer.setPlayerPosition(14);
+        checkIfPropertyIsBought(currentPlayer, currentField);
+    }
+
+    public void freeOrangeOrGreenCard(Player currentPlayer, PropertyField currentField, GUIController GUIController)
+    {
+        String button = GUIController.fourButtons("Gratis felt! Ryk frem til et orange eller grønt felt." +
+                "\nHvis det er ledigt, får du det gratis! " +
+                "\nEllers skal du betale leje til ejeren.", "Skaterparken", "Swimmingpoolen", "Bowlinghallen", "Zoo");
+        if(button.equals("Skaterparken"))
+        {
+            currentPlayer.setPlayerPosition(4);
+        }
+        else if(button.equals("Swimmingpoolen"))
+        {
+            currentPlayer.setPlayerPosition(5);
+        }
+        else if(button.equals("Bowlinghallen"))
+        {
+            currentPlayer.setPlayerPosition(19);
+        }
+        else
+        {
+            currentPlayer.setPlayerPosition(20);
+        }
+        checkIfPropertyIsBought(currentPlayer, currentField);
+    }
+
+    public void freePinkOrBlueCard(Player currentPlayer, PropertyField currentField, GUIController GUIController)
+    {
+        String button = GUIController.fourButtons("Gratis felt! Ryk frem til et pink eller blåt felt." +
+                "\nHvis det er ledigt, får du det gratis! " +
+                "\nEllers skal du betale leje til ejeren.", "Museet", "Biblioteket", "Vandlandet", "Strandpromenaden");
+        if(button.equals("Museet"))
+        {
+            currentPlayer.setPlayerPosition(7);
+        }
+        else if(button.equals("Biblioteket"))
+        {
+            currentPlayer.setPlayerPosition(8);
+        }
+        else if(button.equals("Vandlandet"))
+        {
+            currentPlayer.setPlayerPosition(22);
+        }
+        else
+        {
+            currentPlayer.setPlayerPosition(23);
+        }
         checkIfPropertyIsBought(currentPlayer, currentField);
     }
 }
