5238d28b158a3e16c826a6177666056b10039898
louismonty <louis.mail.dk@gmail.com>
2020-11-22 14:55:38 +0100

    made the function landOnField in PropertyField

--- src/main/java/Game/PropertyField.java
+++ src/main/java/Game/PropertyField.java
@@ -1,5 +1,9 @@
 package Game;
 
+import Controller.FieldController;
+import Controller.GUIController;
+import gui_fields.GUI_Player;
+
 public class PropertyField extends Field
 {
     private int price;
@@ -62,7 +66,27 @@ public class PropertyField extends Field
         else
             return false;
     }
-    public void landOnField()
+    public void landOnField(FieldController FC, Player currentPlayer, GUIController GC, GUI_Player player, GUI_Player[] PlayerArray){
+        PropertyField cf =(PropertyField) FC.getGameboard()[currentPlayer.getPlayerPosition()];
+        if(!cf.getIsPropertyBought()){
+            GC.showMessege("du købte "+cf.getName());
+            if( currentPlayer.getPlayerAccount().getBalance() > cf.getPrice()){
+                GC.makeOwner(currentPlayer.getPlayerPosition(),player.getPrimaryColor());
+                cf.setOwner(currentPlayer);
+                cf.setIsPropertyBought(true);
+                currentPlayer.getPlayerAccount().setBalance(currentPlayer.getPlayerAccount().getBalance()-cf.getPrice());
+            }
+        }
+        else if(cf.getOwner().equals(currentPlayer)) {
+            //
+        }
+        else{
+            GC.showMessege("du skal betale penge til"+cf.getOwner());
+            currentPlayer.getPlayerAccount().setBalance(currentPlayer.getPlayerAccount().getBalance()-cf.getPrice());
+            cf.getOwner().getPlayerAccount().setBalance(cf.getOwner().getPlayerAccount().getBalance()+cf.getRent());
+            GC.updatePlayerBal(cf.getOwner().getPlayerAccount().getBalance(),PlayerArray[cf.getOwner().getPlayerId()]);
+        }
+    })
     {}
 
 }
