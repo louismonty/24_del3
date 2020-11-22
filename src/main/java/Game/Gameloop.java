db498f3fffe4594f97e904b1a9eb10c07fcc9e55
louismonty <louis.mail.dk@gmail.com>
2020-11-22 14:15:14 +0100

    removed ruels and to import from develpment from Rules, in Gameloop i moved stuff into other class to make it more readable

--- src/main/java/Game/Gameloop.java
+++ src/main/java/Game/Gameloop.java
@@ -7,35 +7,27 @@ import java.awt.*;
 
 
 public class Gameloop {
-    public static DiceController Dc = new DiceController();
-public static FieldController FC = new FieldController();
-public static GUIController GC = new GUIController();
-public static PlayerController PC = new PlayerController();
-public static Menu Mn = new Menu();
-public static ruels rl = new ruels();
+    private static DiceController Dc = new DiceController();
+private static FieldController FC = new FieldController();
+private static GUIController GC = new GUIController();
+private static PlayerController PC = new PlayerController();
+private static Menu Mn = new Menu();
+private static ruels rl = new ruels();
 private static boolean isGameRunning = true;
 private static int roll;
 public static void gameloop(){
     Player[] playerArray;
-    int newPlayerPosition;
-    PC.setPlayerArray(Mn.AddPlayer());
+    PC.setPlayerArray(Mn.AddPlayer(GC));
     FC.createGameBoardFields();
     FC.createChanceCardDeck();
     Player currentPlayer = PC.getCurrentPlayer();
     GUI_Player[] GuiPlayers = GC.createplayer(PC.getPlayerArray());
     while(isGameRunning) {
+
         currentPlayer = PC.getCurrentPlayer();
-        GC.oneButton("its" + " " + currentPlayer.getPlayerName() + " " + "to roll click roll", "roll");
-        Dc.diceRoll();
-        roll = Dc.diceValue();
-        GC.showDice(roll);
-        newPlayerPosition = (currentPlayer.getPlayerPosition() + roll) % 24;
-        GC.movePlayer(currentPlayer.getPlayerPosition(), roll, GuiPlayers[currentPlayer.getPlayerId()]);
-        currentPlayer.setPlayerPosition(newPlayerPosition);
-        if (GC.isInstanceOfField(currentPlayer.getPlayerPosition())) {
-            rl.propertyField(FC, currentPlayer, GC, GuiPlayers[currentPlayer.getPlayerId()], GuiPlayers);
-        }
-        rl.afterRuels(currentPlayer);
+        rollDice(currentPlayer,GuiPlayers);
+        FC.getGameboard()[currentPlayer.getPlayerPosition()].landOnField(FC,GC,currentPlayer,GuiPlayers);
+        rl.afterRuels(currentPlayer,GC);
         GuiPlayers[currentPlayer.getPlayerId()].setBalance(currentPlayer.getPlayerAccount().getBalance());
         isGameRunning = rl.win(currentPlayer, GC);
         PC.NextPlayer();
@@ -44,5 +36,13 @@ public static void gameloop(){
     public static void main(String[] args){
     gameloop();
     }
-
+    public static void rollDice(Player currentPlayer, GUI_Player[] GuiPlayers){
+        GC.oneButton("its" + " " + currentPlayer.getPlayerName() + " " + "to roll click roll", "roll");
+        Dc.diceRoll();
+        roll = Dc.diceValue();
+        GC.showDice(roll);
+        int newPlayerPosition = (currentPlayer.getPlayerPosition() + roll) % 24;
+        GC.movePlayer(currentPlayer.getPlayerPosition(), roll, GuiPlayers[currentPlayer.getPlayerId()]);
+        currentPlayer.setPlayerPosition(newPlayerPosition);
+    }
 }
