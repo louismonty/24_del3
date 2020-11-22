91c9aeee9c16dd8cf545fa2f15407d8ea9f78890
louismonty <louis.mail.dk@gmail.com>
2020-11-22 16:01:16 +0100

    added player start balance in PlayerController

--- src/main/java/Controller/PlayerController.java
+++ src/main/java/Controller/PlayerController.java
@@ -75,23 +75,23 @@ public class PlayerController {
 
     public void Make2Players()
     {
-        player1 = new Player("UNKNOWN", 0, new Account(1), Color.black,0);
-        player2 = new Player("UNKNOWN", 0, new Account(1), Color.blue,1);
+        player1 = new Player("UNKNOWN", 0, new Account(20), Color.black,0);
+        player2 = new Player("UNKNOWN", 0, new Account(20), Color.blue,1);
         playerArray = new Player[]{player1, player2};
     }
     public void Make3Players()
     {
-        player1 = new Player("UNKNOWN", 0, new Account(1), Color.black,0);
-        player2 = new Player("UNKNOWN", 0, new Account(1), Color.blue,1);
-        player3 = new Player("UNKNOWN", 0, new Account(1), Color.red,2);
+        player1 = new Player("UNKNOWN", 0, new Account(18), Color.black,0);
+        player2 = new Player("UNKNOWN", 0, new Account(18), Color.blue,1);
+        player3 = new Player("UNKNOWN", 0, new Account(18), Color.red,2);
         playerArray = new Player[]{player1, player2, player3};
     }
     public void Make4Players()
     {
-        player1 = new Player("UNKNOWN", 0, new Account(1), Color.black,0);
-        player2 = new Player("UNKNOWN", 0, new Account(1), Color.blue,1);
-        player3 = new Player("UNKNOWN", 0, new Account(1), Color.red,2);
-        player4 = new Player("UNKNOWN", 0, new Account(1), Color.green,3);
+        player1 = new Player("UNKNOWN", 0, new Account(16), Color.black,0);
+        player2 = new Player("UNKNOWN", 0, new Account(16), Color.blue,1);
+        player3 = new Player("UNKNOWN", 0, new Account(16), Color.red,2);
+        player4 = new Player("UNKNOWN", 0, new Account(16), Color.green,3);
         playerArray = new Player[]{player1, player2, player3, player4};
     }
 }
