01f18759c91e0f6cea6f2b56767d1216bdf1717f
Isak_s205452 <s205452@student.dtu.dk>
2020-11-22 13:15:46 +0100

    added constructor, getName and landOnField methods in CustomField class

--- src/main/java/Game/CustomField.java
+++ src/main/java/Game/CustomField.java
@@ -1,4 +1,18 @@
 package Game;
 
-public class CustomField {
+public class CustomField extends Field
+{
+
+
+    public CustomField(int positionID, String colorID, String name)
+    {
+        super(positionID, colorID,name);
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public void landOnField()
+    {}
+
 }
